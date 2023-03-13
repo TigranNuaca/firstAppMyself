b70864fa2a31d70e6b80fb45c22d1c95d1d20bbf
TigraNuaca <tigrannuaca2020@gmail.com>
2023-03-13 14:56:03 -0700

    First Commit

---
+++ src/main/java/com/autoregistration/firstappmyself/FirstAppMyselfApplication.java
@@ -0,0 +1,13 @@
+package com.autoregistration.firstappmyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppMyselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppMyselfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoregistration/firstappmyself/FirstAppMyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoregistration.firstappmyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppMyselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
